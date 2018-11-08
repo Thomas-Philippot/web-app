bfd49b7a771f1d766b7b0ff889bfb7fb9699ad2a
Thomas <thomas.philippot@labo-mylab.fr>
2018-11-08 15:54:07 +0100

    fontawesome icons, proper loggin check and weather app

---
+++ middleware/login.js
@@ -0,0 +1,8 @@
+export default function ({ redirect }) {
+  // If nuxt generate, pass this middleware
+  if (process.browser) {
+    if (localStorage.loggedIn) {
+      return redirect('/home')
+    }
+  }
+}
